c08a57a4b1c6d34eb6b14f4fd06976ea79068e12
dukinm <dukinm@gmail.com>
2023-11-11 18:10:06 +0300

    DEMO-20 - Создано рабочее окружение

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	_ "database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	//fmt.Fprint(w, psqlInfo)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		fmt.Fprint(w, "{\"Соединение установлено\": false}")
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Fprint(w, "{\"Соединение установлено\": false}")
+	} else {
+		fmt.Fprint(w, "{\"Соединение установлено\": true}")
+	}
+	defer db.Close()
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/api/status", handler)
 	http.ListenAndServe(":80", nil)
 }
